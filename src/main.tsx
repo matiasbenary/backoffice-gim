42f1c05261a031cda9525c19325add0e851195cc
Matias Benary <matiasbenary@gmail.com>
2026-04-13 12:27:35 -0300

    disable StrictMode in production

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+if (import.meta.env.PROD) {
+  root.render(<App />)
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
